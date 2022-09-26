908b52c2b5ab8bd04aa30d965bcf26cc570d06b8
razavioo <razavioo@gmail.com>
2022-09-27 00:51:32 +0330

    Code cleanup

--- music-grabber/src/main/java/dev/emad/music/grabber/GeneralMusicGrabber.kt
+++ music-grabber/src/main/java/dev/emad/music/grabber/GeneralMusicGrabber.kt
@@ -7,52 +7,40 @@ import kotlinx.coroutines.flow.collectLatest
 
 class GeneralMusicGrabber : MusicGrabber() {
     override fun grab(url: String): Flow<MusicInformation> = channelFlow {
-        when (getMusicSource(url)) {
-            MusicSource.MUZIC_IR -> {
-                MuzicIrMusicGrabber().grab(url).collectLatest {
-                    trySend(it)
-                    close()
+        getMusicSource(url)?.let { musicSource ->
+            when (musicSource) {
+                MusicSource.MUZIC_IR -> {
+                    MuzicIrMusicGrabber().grab(url).collectLatest {
+                        trySend(it)
+                        close()
+                    }
+                }
+
+                MusicSource.TARAFDARI -> {
+                    TarafdariMusicGrabber().grab(url).collectLatest {
+                        trySend(it)
+                        close()
+                    }
+                }
+
+                MusicSource.AVAZINO -> {
+                    AvazinoMusicGrabber().grab(url).collectLatest {
+                        trySend(it)
+                        close()
+                    }
                 }
             }
-
-            MusicSource.TARAFDARI -> {
-                TarafdariMusicGrabber().grab(url).collectLatest {
-                    trySend(it)
-                    close()
-                }
-            }
-
-            MusicSource.AVAZINO -> {
-                AvazinoMusicGrabber().grab(url).collectLatest {
-                    trySend(it)
-                    close()
-                }
-            }
-
-            else -> {
-                close()
-            }
+        } ?: run {
+            close()
         }
 
         awaitClose()
     }
 
-    private fun getMusicSource(url: String): MusicSource? {
-        val source: MusicSource? = when {
-            url.startsWith(MusicSource.TARAFDARI.website) -> {
-                MusicSource.TARAFDARI
-            }
-
-            url.startsWith(MusicSource.MUZIC_IR.website) -> {
-                MusicSource.MUZIC_IR
-            }
-
-            url.startsWith(MusicSource.AVAZINO.website) -> {
-                MusicSource.AVAZINO
-            }
-
-            else -> null
-        }
-        return source
+    private fun getMusicSource(url: String): MusicSource? = when {
+        url.startsWith(MusicSource.TARAFDARI.website) -> MusicSource.TARAFDARI
+        url.startsWith(MusicSource.MUZIC_IR.website) -> MusicSource.MUZIC_IR
+        url.startsWith(MusicSource.AVAZINO.website) -> MusicSource.AVAZINO
+        else -> null
     }
 }
