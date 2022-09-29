22aaa7cbcf19cc8cde2710afd67d7ea5c4f0b9d0
razavioo <razavioo@gmail.com>
2022-09-29 13:15:58 +0330

    Fix invalid url response message

--- src/main/kotlin/dev/emad/plugins/Routing.kt
+++ src/main/kotlin/dev/emad/plugins/Routing.kt
@@ -13,6 +13,7 @@ import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.resources.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.json.simple.JSONObject
 import org.koin.ktor.ext.inject
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.SLF4JLogger
@@ -48,7 +49,10 @@ fun Application.configureRouting() {
         get<AudioRequest.RedirectionId> { audio ->
             val existingAudio = audioRepository.getFirstFromRedirectionId(audio.redirectionId)
             if (existingAudio == null) {
-                call.respond(HttpStatusCode.NotFound, "This audio url is not found.")
+                call.respond(
+                    HttpStatusCode.NotFound,
+                    JSONObject(mapOf("message" to "This audio url is not found."))
+                )
             } else {
                 call.respondRedirect(existingAudio.downloadUrl, false)
             }
