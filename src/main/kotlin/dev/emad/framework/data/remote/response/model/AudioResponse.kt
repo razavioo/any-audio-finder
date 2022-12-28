16f037bf6f780b06ea4b306a3b7385b0f39893e9
emad <razavioo@gmail.com>
2022-12-28 23:53:42 +0330

    Improve response namings

--- src/main/kotlin/dev/emad/framework/data/remote/response/model/AudioResponse.kt
+++ src/main/kotlin/dev/emad/framework/data/remote/response/model/AudioResponse.kt
@@ -1,10 +1,13 @@
 package dev.emad.framework.data.remote.response.model
 
+import com.google.gson.annotations.SerializedName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class AudioResponse(
     val id: Long? = null,
+    @SerializedName("page_url")
     val pageUrl: String,
+    @SerializedName("download_url")
     val downloadUrl: String
 )
